7b366037b3f22794408514c1ec32993f072ae5fc
Rakshita Singh <rakshita.vorphy@gmail.com>
2022-10-02 21:54:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'loading_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override 
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
+
