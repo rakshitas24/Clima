d858ad5d01f52e3808eb8d585b7526227664e89c
Rakshita Singh <rakshita.vorphy@gmail.com>
2022-10-03 18:53:07 +0530

    OpenWeatherMap API setup added

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,8 +1,13 @@
-// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors, avoid_print
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors, avoid_print, prefer_typing_uninitialized_variables
 
+import 'package:clima/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'networking.dart';
+
+const apiKey = '1a857c13f49d79a539214ff8c32743d0';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,26 +15,31 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
+  double latitude = 0;
+  double longitude = 0;
 
   @override
-  Widget build (BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    getLocationData();
   }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+        var weatherData = networkHelper.getData();
   }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
